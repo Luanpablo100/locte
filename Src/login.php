78b0a0d35dc2c010a221853df1ce171b6d337f63
Luan Oliveira <lpablo611@gmail.com>
2023-05-02 20:37:45 -0400

    Backend start

---
+++ Src/login.php
@@ -0,0 +1,71 @@
+<?php session_start();
+	// $servidor = "localhost";
+	// $usuario = "root";
+	// $senha = "";
+	// $db_name = "locte_db";
+
+	require('conexao.php');
+
+
+	if (isset($_POST['user_login'])) {      
+	
+		$user_login = $_POST['user_login'];
+		// $passwd_login = md5($_POST['passwd_login']);
+        $passwd_login = $_POST['passwd_login'];
+	
+
+		try {
+			// $conn = mysqli_connect($servidor, $usuario, $senha, $db_name) or die('Banco de dados indisponível.');
+
+			$sql_valida_login = mysqli_query($conexao,"SELECT * FROM conta WHERE usuario = '".$user_login."' AND senha = '".$passwd_login."'");
+		  } catch (\Throwable $th) {
+			throw $th;
+		  }
+		
+		
+		if(mysqli_num_rows($sql_valida_login) > 0){
+	
+		$registros_login = mysqli_fetch_assoc($sql_valida_login);
+		// echo($registros_login['usuario']);
+		// echo($registros_login['senha']);
+		// echo($registros_login['perfil']);
+				 
+			// $_SESSION['nome_completo_login'] = $registros_login['nome_completo_login'];
+			// $_SESSION['nome_completo_login'] = $registros_login['nome_completo_login'];
+			// $_SESSION['nome_login'] = $registros_login['nome_login'];
+			$_SESSION['perfil'] = $registros_login['perfil'];
+
+
+			$_SESSION['url'] = $url;
+			$_SESSION['url_admin'] = $url_admin;
+			$_SESSION['url_cliente'] = $url_cliente;
+					
+			
+				if($_SESSION['perfil'] == "Admin"){
+
+					/*echo "<script> alert('Administrador [LOGADO]');</script>";*/
+
+					echo "<script> window.location.href='$url_admin';</script>";	
+
+
+
+
+				}else{
+
+					/*echo "<script> alert('Cliente [LOGADO]');</script>";*/
+
+					echo "<script> window.location.href='$url_cliente';</script>";
+
+				}
+			
+		} else {
+
+		echo "<script> alert('Erro ao fazer login. Tente novamente ou fale com o Administrador.');</script>";
+
+		echo "<script> window.location.href='$url';</script>";
+			
+		}
+
+	}
+
+?> 
